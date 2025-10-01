77845044449842aa1525cb7bd9f053be75989804
Matías Lorenzo <matete@gmail.com>
2025-10-01 14:27:57 -0300

    feat: add spider enemy

---
+++ Assets/Game/Gameplay/Enemies/Scripts/Spider/SpiderAnimationController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class SpiderAnimationController : MonoBehaviour
+{
+  readonly string animWalk = "Walk";
+  readonly string animAttack = "Attack";
+  readonly string animIdle = "Idle";
+  readonly string animDeath = "Death";
+  readonly string animJump = "Jump"; // Para el estado de daño (Damaged)
+  private Animator anim = null;
+
+  private void Awake()
+  {
+    anim = GetComponent<Animator>();
+  }
+
+  public void ToggleWalk(bool walk)
+  {
+    anim?.SetBool(animWalk, walk);
+  }
+
+  public void ToggleIdle(bool walk)
+  {
+    anim?.SetBool(animIdle, walk);
+  }
+
+  public void TriggerAttack()
+  {
+    anim?.SetTrigger(animAttack);
+  }
+
+  public void TriggerDamage()
+  {
+    anim?.SetTrigger(animJump);
+  }
+
+  public void TriggerDeath()
+  {
+    anim?.SetTrigger(animDeath);
+  }
+
+}
---
+++ Assets/Game/Gameplay/Enemies/Scripts/Spider/SpiderChaseState.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class SpiderChaseState : IEnemyState
+{
+  private readonly SpiderEnemy enemy;
+  public EnemyState State { get; private set; }
+
+  public SpiderChaseState(SpiderEnemy enemy)
+  {
+    this.enemy = enemy;
+    State = EnemyState.Chase;
+  }
+
+  public void Enter()
+  {
+    if (enemy.Animator != null)
+    {
+      enemy.Animator.ToggleWalk(true);
+    }
+  }
+
+  public void Update()
+  {
+    if (enemy.CurrentTarget == null)
+    {
+      enemy.ChangeState(new SpiderIdleState(enemy));
+      return;
+    }
+
+    if (enemy.CurrentTarget != null && enemy.DistanceToTarget() <= enemy.AttackRange)
+    {
+      enemy.ChangeState(new SpiderAttackState(enemy));
+    }
+
+    enemy.MoveTo(enemy.CurrentTarget.position);
+  }
+
+  public void Exit()
+  {
+    if (enemy.Animator != null)
+    {
+      enemy.Animator.ToggleWalk(false);
+    }
+    enemy.StopMovement();
+  }
+}
